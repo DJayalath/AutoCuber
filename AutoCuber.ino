5c9b7e9950ca4f1de1d733c243a875b548a9d2f9
armytricks <dhjayalath@gmail.com>
2019-08-22 10:25:59 +0100

    Using new custom motor driving library

--- AutoCuber.ino
+++ AutoCuber.ino
@@ -1,49 +1,15 @@
-﻿// Define pin connections & motor's steps per revolution
-const int dirPin = 4;
-const int stepPin = 3;
-const int stepsPerRevolution = 200;
+﻿#include "MotorDriver.h"
 
-int steps = 0;
+MotorDriver M1 = MotorDriver(3, 4);
 
 void setup()
 {
-	// Declare pins as Outputs
-	pinMode(stepPin, OUTPUT);
-	pinMode(dirPin, OUTPUT);
 }
+
 void loop()
 {
-	// Set motor direction clockwise
-	digitalWrite(dirPin, HIGH);
-
-	// Spin motor slowly
-	for (int x = 0; x < stepsPerRevolution; x++)
-	{
-		digitalWrite(stepPin, HIGH);
-		delayMicroseconds(1000);
-		digitalWrite(stepPin, LOW);
-		delayMicroseconds(1000);
-		steps++;
-		if (steps >= 200) {
-			steps = 0;
-		}
-	}
-	delay(1000); // Wait a second
-
-	// Set motor direction counterclockwise
-	digitalWrite(dirPin, LOW);
-
-	// Spin motor quickly
-	for (int x = 0; x < stepsPerRevolution; x++)
-	{
-		digitalWrite(stepPin, HIGH);
-		delayMicroseconds(1000);
-		digitalWrite(stepPin, LOW);
-		delayMicroseconds(1000);
-		steps--;
-		if (steps <= -1) {
-			steps = 199;
-		}
-	}
-	delay(1000); // Wait a second
+	M1.Turn(Direction::ACW, 360, 1000);
+	delay(500);
+	M1.Turn(Direction::CW, 360, 1000);
+	delay(500);
 }
