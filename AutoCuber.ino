b99bd562799aa6067ea426e6458827242f0d06b9
armytricks <dhjayalath@gmail.com>
2019-09-05 20:55:34 +0100

    2 motor test code

--- AutoCuber.ino
+++ AutoCuber.ino
@@ -3,6 +3,8 @@
 
 MotorDriver M1 = MotorDriver(3, 4);
 MotorDriver M2 = MotorDriver(9, 8);
+MotorDriver M3 = MotorDriver(10, 11);
+MotorDriver M4 = MotorDriver(12, 13);
 RotaryEncoder R = RotaryEncoder(7, 6, 5);
 int speed = 1000;
 
@@ -27,6 +29,18 @@ void loop()
 	//}
 
 	M2.Turn(Direction::ACW, 360, 1000);
+	delay(500);
+	M2.Turn(Direction::CW, 720, 1000);
+	delay(500);
 	M1.Turn(Direction::ACW, 50 * 8, 1000);
+	delay(100);
 	M1.Turn(Direction::CW, 50 * 8, 1000);
+
+	M3.Turn(Direction::ACW, 360, 1000);
+	delay(500);
+	M3.Turn(Direction::CW, 720, 1000);
+	delay(500);
+	M4.Turn(Direction::ACW, 50 * 8, 1000);
+	delay(100);
+	M4.Turn(Direction::CW, 50 * 8, 1000);
 }
