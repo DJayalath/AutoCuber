7293cd6ed57530061f4bee138909228619b82949
armytricks <dhjayalath@gmail.com>
2019-09-06 15:15:28 +0100

    5-Motor drive test

--- AutoCuber.ino
+++ AutoCuber.ino
@@ -1,11 +1,16 @@
 ﻿#include "MotorDriver.h"
 #include "RotaryEncoder.h"
 
-MotorDriver M1 = MotorDriver(3, 4);
-MotorDriver M2 = MotorDriver(9, 8);
-MotorDriver M3 = MotorDriver(10, 11);
-MotorDriver M4 = MotorDriver(12, 13);
-RotaryEncoder R = RotaryEncoder(7, 6, 5);
+MotorDriver M1 = MotorDriver(2, 3);
+MotorDriver M2 = MotorDriver(4, 5);
+MotorDriver M3 = MotorDriver(6, 7);
+MotorDriver M4 = MotorDriver(8, 9);
+MotorDriver M5 = MotorDriver(10, 11);
+//MotorDriver M3 = MotorDriver(10, 11);
+//MotorDriver M4 = MotorDriver(12, 13);
+//MotorDriver M5 = MotorDriver(23, 22);
+//MotorDriver M6 = MotorDriver(24, 25);
+//RotaryEncoder R = RotaryEncoder(7, 6, 5);
 int speed = 1000;
 
 void setup()
@@ -28,13 +33,36 @@ void loop()
 	//	speed -= 100;
 	//}
 
-	M2.Turn(Direction::ACW, 360, 1000);
+	//M1.Turn(Direction::ACW, 50 * 8, 1000);
+	//delay(100);
+	//M2.Turn(Direction::CW, 360, 600);
+	//delay(100);
+	//M1.Turn(Direction::CW, 50 * 8, 1000);
+	//delay(100);
+
+	////M4.Turn(Direction::ACW, 50 * 8, 1000);
+	////delay(100);
+	//M3.Turn(Direction::CW, 360, 600);
+	//delay(100);
+	////M4.Turn(Direction::CW, 50 * 8, 1000);
+	////delay(100);
+
+	//M6.Turn(Direction::ACW, 50 * 8, 1000);
+	//delay(100);
+	////M5.Turn(Direction::CW, 360, 600);
+	////delay(100);
+	//M6.Turn(Direction::CW, 50 * 8, 1000);
+	//delay(100);
+	
+
+	M1.Turn(Direction::ACW, 360, 1000);
 	delay(500);
-	M2.Turn(Direction::CW, 720, 1000);
+	M1.Turn(Direction::CW, 720, 1000);
 	delay(500);
-	M1.Turn(Direction::ACW, 50 * 8, 1000);
+	M2.Turn(Direction::ACW, 50 * 8, 1000);
 	delay(100);
-	M1.Turn(Direction::CW, 50 * 8, 1000);
+	M2.Turn(Direction::CW, 50 * 8, 1000);
+	delay(100);
 
 	M3.Turn(Direction::ACW, 360, 1000);
 	delay(500);
@@ -43,4 +71,10 @@ void loop()
 	M4.Turn(Direction::ACW, 50 * 8, 1000);
 	delay(100);
 	M4.Turn(Direction::CW, 50 * 8, 1000);
+	delay(100);
+
+	M5.Turn(Direction::ACW, 360, 1000);
+	delay(500);
+	M5.Turn(Direction::CW, 720, 1000);
+	delay(500);
 }
